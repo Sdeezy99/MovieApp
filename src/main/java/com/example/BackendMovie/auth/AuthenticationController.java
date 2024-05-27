bd2d9509ae8539dc09e09b35d0bb8064acddda66
sihle1996 <mofokengsihle63@gmail.com>
2024-05-27 11:23:37 +0200

    ..

--- src/main/java/com/example/BackendMovie/auth/AuthenticationController.java
+++ src/main/java/com/example/BackendMovie/auth/AuthenticationController.java
@@ -2,16 +2,13 @@ package com.example.BackendMovie.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
-
-    @RestController
+@RestController
     @RequestMapping("/api/v1/auth")
     @RequiredArgsConstructor
+    @CrossOrigin("*")
     public class AuthenticationController {
 
         private final AuthenticationService service;
